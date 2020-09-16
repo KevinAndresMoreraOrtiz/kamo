7c8984aa7fdf6e24404b3e28132bae5d08ebdcaf
Kevin Andres Morera Ortiz <49798095+KevinAndresMoreraOrtiz@users.noreply.github.com>
2020-09-15 21:56:35 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+const mongoose = require('mongoose');
+const app = require('./app');
+const port = 3700;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/portafolio')
+        .then(() => {
+        	console.log("Conexión a la base de datos establecida satisfactoriamente...");
+
+        	// Creacion del servidor
+        	app.listen(port, () => {
+        		console.log("Servidor corriendo correctamente en la url: localhost:3700");
+        	});
+
+        })
+		.catch(err => console.log(err));
+
